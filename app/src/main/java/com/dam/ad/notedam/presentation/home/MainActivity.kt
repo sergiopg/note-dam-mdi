b5bdc700b1e132f0d2b57441130cb9b1ded9f265
SergioPG <sergio.palomo7@educa.madrid.org>
2023-12-01 10:47:32 +0100

    Al ejectutar sale un dialog de  si lo quieres guardar en local o remoto

--- app/src/main/java/com/dam/ad/notedam/presentation/home/MainActivity.kt
+++ app/src/main/java/com/dam/ad/notedam/presentation/home/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.dam.ad.notedam.presentation.home
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
@@ -8,6 +10,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.dam.ad.notedam.R
 import com.dam.ad.notedam.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -20,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initNavMenu()
+        showSaveFileDialog()
     }
 
     private fun initNavMenu() {
@@ -28,4 +32,33 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavView.setupWithNavController(navController)
     }
 
+
+    private fun showSaveFileDialog() {
+        val options = arrayOf("Local", "Remoto")
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Guardar fichero:")
+            .setItems(options) { dialogInterface: DialogInterface, which: Int ->
+                when (which) {
+                    0 -> saveLocally()
+                    1 -> saveRemotely()
+                }
+                dialogInterface.dismiss()
+            }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun saveLocally() {
+        // Aquí puedes implementar la lógica para guardar localmente
+        // por ejemplo, escribir en un archivo en el almacenamiento interno
+        val file = File(filesDir, "nombre_del_archivo.csv")
+        // Realiza la operación de escritura en el archivo local
+    }
+
+    private fun saveRemotely() {
+        // Aquí puedes implementar la lógica para guardar de forma remota
+        // por ejemplo, enviar los datos a un servidor
+    }
 }
